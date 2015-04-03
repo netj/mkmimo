d0627e8e74acdcdbf61199b29476d89133202ddc
Jaeho Shin <netj@sparcs.org>
2015-04-03 14:46:34 -0700

    Cleaned up interfaces with Inputs and Outputs types

--- mkmimo.c
+++ mkmimo.c
@@ -14,9 +14,10 @@ typedef struct circular_buffer {
     // the byte range that contains data
     int begin, end;
 } Buffer;
+struct input;
 typedef struct record {
-    // the buffer containing the record
-    Buffer *buffer;
+    // the input the record originates from
+    struct input *input;
     // the byte range of the record in that buffer
     int begin, end;
 } Record;
@@ -31,6 +32,11 @@ typedef struct input {
     // the first few records in the buffer
     Record records[10];
 } Input;
+typedef struct {
+    Input *inputs;
+    int num_inputs;
+    int num_closed;
+} Inputs;
 typedef struct output {
     // output file descriptor
     int fd;
@@ -39,13 +45,17 @@ typedef struct output {
     // queue of records to write
     Record *records[10];
 } Output;
+typedef struct {
+    Output *outputs;
+    int num_outputs;
+    int num_busy;
+    int last_written_output;
+} Outputs;
 
 static char NAME_FOR_STDIN[] = "-";
 static char NAME_FOR_STDOUT[] = "-";
 
-int parse_arguments(int argc, char *argv[], int *num_inputs,
-                    struct input **inputs, int *num_outputs,
-                    struct output **outputs) {
+int parse_arguments(int argc, char *argv[], Inputs *inputs, Outputs *outputs) {
     // first, count number of input/output arguments
     int num_in = 0, num_out = 0;
     int base_idx_out = 0;
@@ -61,15 +71,15 @@ int parse_arguments(int argc, char *argv[], int *num_inputs,
     // open them
     if (num_in == 0) {
         // default to stdin
-        *num_inputs = 1;
-        *inputs = calloc(1, sizeof(struct input));
+        inputs->num_inputs = 1;
+        inputs->inputs = calloc(1, sizeof(struct input));
         struct input this = {
             .fd = 0, .name = NAME_FOR_STDIN,
         };
-        **inputs = this;
+        inputs->inputs[0] = this;
     } else {
-        *num_inputs = num_in;
-        *inputs = calloc(num_in, sizeof(Input));
+        inputs->num_inputs = num_in;
+        inputs->inputs = calloc(num_in, sizeof(Input));
         for (int i = 0; i < num_in; ++i) {
             char *name = argv[i];
             int fd = open(name, O_RDONLY | O_NONBLOCK);
@@ -77,20 +87,20 @@ int parse_arguments(int argc, char *argv[], int *num_inputs,
             Input this = {
                 .fd = fd, .name = name,
             };
-            (*inputs)[i] = this;
+            inputs->inputs[i] = this;
         }
     }
     if (num_out == 0) {
         // default to stdout
-        *num_outputs = 1;
-        *outputs = calloc(1, sizeof(Output));
+        outputs->num_outputs = 1;
+        outputs->outputs = calloc(1, sizeof(Output));
         Output this = {
             .fd = 1, .name = NAME_FOR_STDOUT,
         };
-        **outputs = this;
+        outputs->outputs[0] = this;
     } else {
-        *num_outputs = num_out;
-        *outputs = calloc(num_out, sizeof(Output));
+        outputs->num_outputs = num_out;
+        outputs->outputs = calloc(num_out, sizeof(Output));
         for (int i = 0; i < num_out; ++i) {
             char *name = argv[base_idx_out + i];
             int fd = open(name, O_WRONLY | O_NONBLOCK | O_CREAT,
@@ -99,13 +109,13 @@ int parse_arguments(int argc, char *argv[], int *num_inputs,
             Output this = {
                 .fd = fd, .name = name,
             };
-            (*outputs)[i] = this;
+            outputs->outputs[i] = this;
         }
     }
     return 0;
 }
 
-int read_records_from(Input *inputs, int num_inputs, int *num_closed) {
+int read_records_from(Inputs *inputs) {
     // TODO select/poll/epoll/kqueue on them
     // TODO read from available inputs
     // TODO count available records
@@ -120,52 +130,46 @@ int has_next_record(Input *input, Record **record) {
     return whether_input_has_a_record;
 }
 
-int route_record_to_an_output(Record *record, Output *outputs, int num_outputs,
-                              int *last_written_output) {
+int route_record_to_an_output(Record *record, Outputs *outputs) {
     // TODO find the next less busy output
     // TODO buffer given record to that output
     return 0;
 }
 
-int write_pending_records_to(Output *outputs, int num_outputs) {
+int write_pending_records_to(Outputs *outputs) {
     // TODO write to each output its buffered records
     int num_outputs_pending = 0;
     return num_outputs_pending;
 }
 
 int main(int argc, char *argv[]) {
-    int num_inputs, num_outputs;
-    Input *inputs;
-    Output *outputs;
-    if (parse_arguments(argc, argv, &num_inputs, &inputs, &num_outputs,
-                        &outputs)) {
+    Inputs inputs;
+    Outputs outputs;
+    if (parse_arguments(argc, argv, &inputs, &outputs)) {
         perror("mkmimo");
         return 1;
     }
 
-    printf("num_inputs: %d\n", num_inputs);
-    printf("num_outputs: %d\n", num_outputs);
+    printf("num_inputs: %d\n", inputs.num_inputs);
+    printf("num_outputs: %d\n", outputs.num_outputs);
 
-    int num_closed = 0, num_busy = 0;
-    int output_schedule = 0;
     Record *record;
-    while (num_closed < num_inputs || num_busy > 0) {
+    while (inputs.num_closed < inputs.num_inputs || outputs.num_busy > 0) {
         // read some
-        if (num_closed < num_inputs) {
+        if (inputs.num_closed < inputs.num_inputs) {
             // TODO with back-pressure (unless all output is busy/blocked)
             int num_records =
-                read_records_from(inputs, num_inputs, &num_closed);
-            for (int i = 0; num_records > 0 && i < num_inputs; ++i) {
-                Input *input = &inputs[i];
+                read_records_from(&inputs);
+            for (int i = 0; num_records > 0 && i < inputs.num_inputs; ++i) {
+                Input *input = &inputs.inputs[i];
                 while (num_records > 0 && has_next_record(input, &record)) {
-                    route_record_to_an_output(record, outputs, num_outputs,
-                                              &output_schedule);
+                    route_record_to_an_output(record, &outputs);
                     --num_records;
                 }
             }
         }
         // write some
-        num_busy = write_pending_records_to(outputs, num_outputs);
+        write_pending_records_to(&outputs);
     }
 
     // TODO cleanup: close all outputs and inputs
