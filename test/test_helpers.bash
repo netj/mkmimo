e9d6d9c9b38a5c6f68106e4b862d60e6eb39cf50
Jaeho Shin <netj@cs.stanford.edu>
2016-03-17 21:39:07 -0700

    Improves regex to capture num bytes from dd output
    and mutes DEBUG output when measuring throughput

--- test/test_helpers.bash
+++ test/test_helpers.bash
@@ -21,7 +21,7 @@ teardown() {
 # sums up all `N bytes` printed by dd(1) from executing given command
 sum_dd_bytes() {
     {
-        "$@" 2>&1 | grep ' bytes' | awk '{print $1}' | tr '\n' +
+        "$@" 2>&1 | grep '^[0-9][0-9]* bytes \(.*\) copied, ' | awk '{print $1}' | tr '\n' +
         echo 0
     } | bc
 }
